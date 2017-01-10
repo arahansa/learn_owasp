41861b5bf9a2c37754c893ad0c01414ae22268b4
arahansa <arahansa@naver.com>
2017-01-11 00:34:14 +0900

    add code

---
+++ src/main/java/com/arahansa/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.arahansa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by jarvis on 2017. 1. 11..
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+  // 1.2. http 설정
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/**").permitAll();
+
+    http.formLogin().loginPage("/login").permitAll();
+  }
+
+
+}
